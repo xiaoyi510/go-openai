af378db8c8a26bb60f491a27f563f1fe91b0eae5
vSpear <loveyybin@qq.com>
2023-11-18 17:39:26 +0800

    Update fine_tuning_job_test.go

--- fine_tuning_job_test.go
+++ fine_tuning_job_test.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/sashabaranov/go-openai"
-	"github.com/sashabaranov/go-openai/internal/test/checks"
+	"github.com/xiaoyi510/go-openai"
+	"github.com/xiaoyi510/go-openai/internal/test/checks"
 )
 
 const testFineTuninigJobID = "fine-tuning-job-id"

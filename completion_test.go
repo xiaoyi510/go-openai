160246cf0b4fc1573a630e2526360001dd89002e
vSpear <loveyybin@qq.com>
2023-11-18 17:40:52 +0800

    Update completion_test.go

--- completion_test.go
+++ completion_test.go
@@ -12,8 +12,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/sashabaranov/go-openai"
-	"github.com/sashabaranov/go-openai/internal/test/checks"
+	"github.com/xiaoyi510/go-openai"
+	"github.com/xiaoyi510/go-openai/internal/test/checks"
 )
 
 func TestCompletionsWrongModel(t *testing.T) {

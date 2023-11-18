75932545dac6a858afd5b73e13c7eed2a606d648
vSpear <loveyybin@qq.com>
2023-11-18 17:42:00 +0800

    Update api_integration_test.go

--- api_integration_test.go
+++ api_integration_test.go
@@ -9,9 +9,9 @@ import (
 	"os"
 	"testing"
 
-	"github.com/sashabaranov/go-openai"
-	"github.com/sashabaranov/go-openai/internal/test/checks"
-	"github.com/sashabaranov/go-openai/jsonschema"
+	"github.com/xiaoyi510/go-openai"
+	"github.com/xiaoyi510/go-openai/internal/test/checks"
+	"github.com/xiaoyi510/go-openai/jsonschema"
 )
 
 func TestAPI(t *testing.T) {
